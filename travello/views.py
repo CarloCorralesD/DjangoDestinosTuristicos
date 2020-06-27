2ccce90edb5c146c1322d398fb9f0e87ef5ba5ed
Carlo Corrales Delgado <64241446+CarloCorralesD@users.noreply.github.com>
2020-06-26 23:38:09 -0500

    only Arequipa is in the web

--- travello/views.py
+++ travello/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from .models import Destination
 
 # Create your views here.
 
 def index(request):
-	return render(request,"index.html",{})
+	dest1 = Destination()
+	dest1.name = "Arequipa"
+	dest1.desc = "La ciudad blanca"
+	dest1.img = 'arequipa.jpg'
+	dest1.price = 90.05
+
+	context = {
+		'dest1' : dest1,
+	}
+	return render(request,"index.html",context)
