078aa5384193256f900d610c618ebcb9741b1cde
Carlo Corrales D <ccorrales@unsa.edu.pe>
2020-06-28 15:31:49 -0500

    Login done

--- login/views.py
+++ login/views.py
@@ -23,6 +23,7 @@ def register(request):
 				user = User.objects.create_user(username=username, password=password1,first_name=first_name,last_name=last_name,email=email)
 				user.save()
 				messages.info(request,"usuario creado")
+				return redirect("login")
 		else:
 			messages.info(request,"la contraseña no coincide durante la confirmación")
 			return redirect("register")
@@ -31,3 +32,18 @@ def register(request):
 	else:
 		context = {}
 		return render(request,"register.html",context)
+
+def login(request):
+	if request.method == "POST":
+		username = request.POST['username']
+		password = request.POST['password']
+		user = auth.authenticate(username=username, password=password)
+		if user is not None:
+			auth.login(request,user)
+			return redirect("/")
+		else:
+			messages.info(request,"Usuario o Contraseña errados")
+			return redirect("login")
+	else:
+		context = {} 
+		return render(request,"login.html",context)
