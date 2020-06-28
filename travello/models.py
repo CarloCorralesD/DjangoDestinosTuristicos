17a634dd3dbe1f35572b0ff8b6ddd6feb179b1f2
Carlo Corrales D <ccorrales@unsa.edu.pe>
2020-06-27 19:14:45 -0500

    Destination model created, Arequipa with img as CharField

--- travello/models.py
+++ travello/models.py
@@ -2,10 +2,9 @@ from django.db import models
 
 # Create your models here.
 
-class Destination:
-	id : int
-	name : str
-	img : str
-	desc : str
-	offer : bool
-	price : int
+class Destination(models.Model):
+	name = models.CharField(max_length=100)
+	img = models.CharField(max_length=100) #ImageField(upload_to='pics') #TextField() 
+	desc = models.TextField()
+	offer = models.BooleanField(default=False)
+	price = models.IntegerField()
